7f325b86c325df3326b5b7d329f29536b4d66d49
Leonel wetzel <wetzel.leonel.gabriel@gmail.com>
2026-02-22 20:16:07 -0300

    FEAT: Implementación de helper de request para parseo y validación de datos

---
+++ api/src/Core/Request.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Core;
+
+class Request {
+
+    /**
+     * Recuperar JSON enviado en la solicitud
+     * @return array
+     */
+    public static function getBody(): array {
+        return json_decode(file_get_contents('php://input'), true) ?? [];
+    }
+
+    /**
+     * Validar datos recibidos en una solicitud
+     * @param array $body // Cuerpo
+     * @param array $camposRequeridos // Campos con tipo de dato incluido ['nombre' => string]
+     * @return string[]
+     */
+    public static function validarRequest($camposRequeridos,$body = null): array{
+
+        // Si no recibo body, lo recupero
+        if(!$body){
+            $body = self::getBody();
+        }
+
+        $errores = [];
+
+        // Recorro los campos requeridos para verificar su existencia
+        foreach ($camposRequeridos as $campo => $tipo) {
+
+            // Si no recibo el campo, lo sumo al error
+            if( !isset($body[$campo]) || $body[$campo] === ''){
+                $errores[] = "El campo '$campo' es requerido ";
+                continue;
+            }
+
+            // Parseo el valor al tipo de dato requerido y valido que tenga sentido
+            switch ($tipo) {
+                case 'string':
+                    $body[$campo] = trim((string)$body[$campo]);
+
+                    if (strlen($body[$campo]) < 1) {
+                        $errores[] = "El campo '$campo' no puede estar vacío.";
+                    }
+                    break;
+
+                case 'int':
+                    $valorFiltrado = filter_var($body[$campo], FILTER_VALIDATE_INT);
+                    if ($valorFiltrado === false) {
+                        $errores[] = "El campo '$campo' debe ser un número entero.";
+                    } else {
+                        $body[$campo] = $valorFiltrado;
+                    }
+                    break;
+                    
+                case 'float':
+                    $valorFiltrado = filter_var($body[$campo], FILTER_VALIDATE_FLOAT);
+
+                    if ($valorFiltrado === false) {
+                        $errores[] = "El campo '$campo' debe ser un número decimal válido.";
+                    } else {
+                        $body[$campo] = $valorFiltrado;
+                    }
+                    break;
+                
+                default:
+                    break;
+            }
+
+
+        }
+
+        return ['errores' => $errores, 'datos' => $body];
+
+    }
+
+}
