86b3012c1a59bfc7cb300ef0a9881428483c3bce
Leonel wetzel <wetzel.leonel.gabriel@gmail.com>
2026-02-22 14:33:41 -0300

    CHORE: Consumo de variables declaradas en el .env

---
+++ api/src/Core/constants.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_NAME', getenv('DB_NAME'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASS', getenv('DB_PASS'));
+define('PRECIO_USD', (float) getenv('PRECIO_USD'));
