2ec18688d9db2927512d98dc4707194b21a6d351
Leonel wetzel <wetzel.leonel.gabriel@gmail.com>
2026-02-22 22:06:37 -0300

    FEAT: Mejoras en los mensajes de respuestas de la api

--- api/src/Views/RespuestasJSON.php
+++ api/src/Views/RespuestasJSON.php
@@ -9,16 +9,22 @@ class RespuestasJSON {
      * @param int $code
      * @return void
      */
-    public static function respuesta($mensaje, bool $estado = true, $datos = null, int $code = 200): void {
+    public static function respuesta($detalle, bool $estado = true, $datos = null, int $code = 200): void {
 
         # Seteo el codigo de respuesta de la solicitud
         http_response_code($code);
 
         # Armo la respuesta
         $respuesta = [
-            "resultado" => $estado,
-            'mensaje' => $mensaje,
+            "resultado" => $estado
         ];
+
+        # Manejo la clave del detalle segun el estado de la operación
+        if($estado){
+            $respuesta['mensaje'] = $detalle;
+        }else{
+            $respuesta['errores'] = $detalle;
+        }
 
         # Si hay datos a incluir lo sumo en el array
         if($datos){
